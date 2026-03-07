d79209911a12663271a5e9953f7e6a5d845db3a1
Christian <wasserballer1@hotmail.de>
2026-03-07 13:25:41 +0100

    Initial commit: Spring Boot Projekt

---
+++ src/main/java/de/thwildau/bibinfo/abgabe_gruppe2/AbgabeGruppe2Application.java
@@ -0,0 +1,13 @@
+package de.thwildau.bibinfo.abgabe_gruppe2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbgabeGruppe2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbgabeGruppe2Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/thwildau/bibinfo/abgabe_gruppe2/AbgabeGruppe2ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thwildau.bibinfo.abgabe_gruppe2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbgabeGruppe2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
